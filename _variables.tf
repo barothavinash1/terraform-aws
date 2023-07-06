05152534f59d6b0bed4350eb061104d228256bda
Avinash Baroth <avinashbaroth@avinashs-mbp.mynetworksettings.com>
2023-07-05 22:35:05 -0400

    added eks tags

---
+++ _variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "cluster_name" {
+  type = string
+}
+variable "cidr" {
+  type = string
+}
+variable "azs" {
+  description = "A list of AZs inside the VPC"
+  type        = list(string)
+  default     = []
+}
+variable "private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
+variable "public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  type        = list(string)
+  default     = []
+}
